bd3a279cea98fcfba7774a3ddb4fa57eef11db78
nguyenhoailinh810 <nguyenhoailinh.dmx@gmail.com>
2024-10-29 14:29:50 +0700

    Add files via upload

---
+++ THỰC HÀNH /TUẦN 3/Linh107823.java
@@ -0,0 +1,26 @@
+package buoi3; 
+public class Linh107823 {
+    public static void main(String[] args) {
+        String maSinhVien = "107823";
+        String hoTen = "Nguyễn Hoài Linh";
+        String ngaySinh = "25/10/2006";
+        int tuoi = 18;
+        boolean gioiTinh = true;
+        String lop = "GD24A";
+        String khoa = "Mỹ thuật ứng dụng";
+        String diaChi = "Cẩm Lệ TP Đà Nẵng";
+
+        System.out.println("-----THÔNG TIN SINH VIÊN-----");
+        System.out.println("Mã sinh viên: " + maSinhVien);
+        System.out.println("Họ và tên: " + hoTen);
+        System.out.println("Ngày sinh: " + ngaySinh);
+        System.out.println("Tuổi: " + tuoi);
+        System.out.println("Giới tính: " + (gioiTinh ? "Nam" : "Nữ"));
+        System.out.println("Lớp: " + lop);
+        System.out.println("Khoa: " + khoa);
+        System.out.println("Địa chỉ: " + diaChi);
+        System.out.println("-----HẾT-----");
+    }
+}
+
+    
